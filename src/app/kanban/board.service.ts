1e1667ef1234b98357b2a6fa87968348802e9b75
Gisela Lanciani <giselalanciani@gmail.com>
2023-09-01 11:15:19 -0300

    modified board service

--- src/app/kanban/board.service.ts
+++ src/app/kanban/board.service.ts
@@ -45,4 +45,26 @@ export class BoardService {
         tasks: firebase.firestore.FieldValue.arrayRemove(task),
       });
   }
+  getUserBoards() {
+    return this.afAuth.authState.pipe(
+      switchMap((user) => {
+        if (user) {
+          return this.db
+            .collection<Board>('boards', (ref) =>
+              ref.where('uid', '==', user.uid).orderBy('priority')
+            )
+            .valueChanges({ idField: 'id' });
+        } else {
+          return [];
+        }
+      })
+    );
+  }
+  sortBoards(boards: Board[]) {
+    const db = firebase.firestore();
+    const batch = db.batch();
+    const refs = boards.map((b) => db.collection('boards').doc(b.id));
+    refs.forEach((ref, idx) => batch.update(ref, { priority: idx }));
+    batch.commit();
+  }
 }
