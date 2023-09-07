8151eeef9fbcb1e6f57807b86f75540f383855d2
Gisela Lanciani <giselalanciani@gmail.com>
2023-09-07 09:48:58 -0300

    modification task dialog

--- src/app/kanban/board/board.component.ts
+++ src/app/kanban/board/board.component.ts
@@ -22,11 +22,19 @@ export class BoardComponent {
     const dialogRef = this.dialog.open(TaskDialogComponent, {
       width: '500px',
       data: task
-        ? { task: { ...task }, isNew: false, boardId: this.board.id, idx }
-        : { task: newTask, isNew: true },
+        ? {
+            task: { ...task },
+            isNew: false,
+            boardId: this.board.id,
+            idx,
+          }
+        : {
+            task: newTask,
+            isNew: true,
+          },
     });
 
-    dialogRef.afterClosed().subscribe((result: any) => {
+       dialogRef.afterClosed().subscribe((result: any) => {
       if (result) {
         if (result.isNew) {
           this.boardService.updateTasks(this.board.id, [
@@ -34,8 +42,7 @@ export class BoardComponent {
             result.task,
           ]);
         } else {
-          const update = this.board.tasks;
-          update.splice(result.idx, 1, result.task);
+          this.board.tasks.splice(result.idx, 1, result.task);
           this.boardService.updateTasks(this.board.id, this.board.tasks);
         }
       }
